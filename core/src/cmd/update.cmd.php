56c6bd84c769fe96a3a2a984499da79c2cf5afb5
Jowi <joaobalaniuc>
2023-01-20 19:17:03 -0300

    beta

--- core/src/cmd/update.cmd.php
+++ core/src/cmd/update.cmd.php
@@ -4,7 +4,7 @@ class update extends cmd
     public function __construct()
     {
         $repo = "https://github.com/appixar/arion.git";
-        $repo_env = "https://raw.githubusercontent.com/appixar/arion/main/.env";
+        $repo_env = "https://raw.githubusercontent.com/appixar/arion/main/manifest.json";
 
         // CURRENT VERSION
         $version = $_ENV['arion']['version'];
@@ -13,18 +13,19 @@ class update extends cmd
 
         // LAST VERSION
         $content = file_get_contents($repo_env);
-        $env = parse_ini_string($content, true);
-        $lastVersion = $env['arion']['version'];
+        /*$env = parse_ini_string($content, true);
+        $lastVersion = $env['arion']['version'];*/
+        $json = json_decode($content, true);
+        $lastVersion = $json['version'];
 
         if ($lastVersion > $version) {
-            $this->say("New version found: $lastVersion");
+            $this->say("New version found: $lastVersion", false, "green");
+            // CREATE DIR
+            //$dir = self::DIR_MODULES . "/$module/";
+            shell_exec("mkdir .tmp");
+            shell_exec("git clone $repo .tmp"); //2>&1
+            shell_exec("cp -R .tmp/* ./");
+            shell_exec("rm -rf .tmp/");
         } else $this->say("You are up to date.");
-
-        // CREATE DIR
-        //$dir = self::DIR_MODULES . "/$module/";
-        //shell_exec("mkdir .tmp");
-        //shell_exec("git clone $url .tmp"); //2>&1
-        //shell_exec("cp -R .tmp/* ./");
-        //shell_exec("rm -rf .tmp/");
     }
 }
