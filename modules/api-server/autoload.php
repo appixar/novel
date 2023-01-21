bcf5424c8389c19c2955cb4b6f2c466a98a5c134
Jowi <joaobalaniuc>
2023-01-21 17:16:52 -0300

    upd ignore

--- modules/api-server/autoload.php
+++
@@ -1,5 +0,0 @@
-<?php
-require("src/apiServer.php");
-require("src/apiServer.http.php");
-require("src/apiServer.auth.php");
-require("src/license.array.php");
--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-print_r(getDirContents('.'));
-// get .env data
-$_ENV = parse_ini_file('.env', true);
-print_r($_ENV);
