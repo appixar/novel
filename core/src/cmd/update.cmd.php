82a25becc8387be8d0f7f73f7031e2eaefcb2b1c
Jowi <joaobalaniuc>
2023-01-21 11:10:18 -0300

    beta

--- core/src/cmd/update.cmd.php
+++ core/src/cmd/update.cmd.php
@@ -9,22 +9,38 @@ class update extends cmd
     {
         // CURRENT VERSION
         global $_MAN;
-        $version = $_MAN['version'];
+        $version = @$_MAN['version'];
+        $sha = @$_MAN['commit']['sha'];
         $this->say("Arion current version: $version");
         $this->say("Looking for updates...");
 
-        // LAST VERSION
+        $updateNow = false;
+
+        // 1. CHECK LAST VERSION
         $json = json_decode(file_get_contents(self::MANIFEST_URL), true);
         $lastVersion = $json['version'];
         $lastUpdatedFiles = $json['updatedFiles'];
-
         if ($lastVersion > $version) {
             $this->say("New version found: $lastVersion", false, "green");
+            $updateNow++;
+        }
+        // OR... 2. CHECK LAST COMMIT DATE
+        else {
+            $json = json_decode(file_get_contents(self::COMMITS_URL), true);
+            $lastSha = @$json[0]['commit']['sha'];
+            $lastDate = @$json[0]['commit']['author']['date'];
+            if ($lastSha != $sha) {
+                $this->say("New commit detected: $lastDate", false, "green");
+                $this->say("SHA: $lastSha", false, "blue");
+                $updateNow++;
+            }
+        }
+        if ($updateNow) {
             // CREATE DIR
             shell_exec("mkdir .tmp");
             shell_exec("git clone " . self::REPO_URL . " .tmp"); //2>&1
             foreach ($lastUpdatedFiles as $file) {
-                $this->say("Copying: $file ...", false, "green");
+                $this->say("Copying: '$file' ...", false, "green");
                 if ($file === '.') exec("cp .tmp/* ./");
                 else shell_exec("cp -R .tmp/$file ./");
             }
