268ea4fe9ed128b56397df09d0179edd781da37b
Jowi <joaobalaniuc>
2023-09-06 18:45:07 -0300

    beta

--- core/mason/mason.serve.php
+++ core/mason/mason.serve.php
@@ -9,11 +9,13 @@ class serve extends Mason
     }
     private function run($port)
     {
-        $public_path = realpath(__DIR__ . "/../../public");
+        //$public_path = realpath(__DIR__ . "/../../public");
+        $server_router = realpath(__DIR__ . "/../src/server/router.php");
         $this->say("");
         $this->say("Novel Web Server", true, "green");
         $this->say("Listening at http://localhost:$port");
         $this->say("-");
-        shell_exec("php -S localhost:$port -t $public_path");
+        //shell_exec("php -S localhost:$port -t $public_path");
+        shell_exec("php -S localhost:$port $server_router");
     }
 }
