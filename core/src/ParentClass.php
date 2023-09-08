80f7b6d0d3caa9ae0a423eb02520813140440cec
Jowi <joaobalaniuc>
2023-09-08 00:23:54 -0300

    beta

--- core/src/ParentClass.php
+++ core/src/ParentClass.php
@@ -4,17 +4,24 @@ class Routes extends Arion
     public $error = false;
     public $error_code = false;
     public $res = false;
-    //
+    // error return
     public function error($error = '', $error_code = 406)
     {
         $this->error = $error;
         $this->error_code = $error_code;
         return false;
     }
+    // success return
     public function res($data = [])
     {
         $this->res = $data;
         return $this->res;
+    }
+    // controller return
+    public function return($objectFromController) {
+        if ($objectFromController->error) return $this->error($objectFromController->error);
+        elseif ($objectFromController->res) return $this->res($objectFromController->res);
+        else return false;
     }
 }
 class Controllers extends Arion
